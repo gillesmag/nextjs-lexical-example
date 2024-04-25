c0320a64cb497a48a9a3c2e8a344a9091a55f186
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2024-04-25 19:48:07 +0200

    minimal working example

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { resolve } from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.resolve.alias.yjs = resolve("./node_modules/yjs/dist/yjs.mjs");
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/components/editor.tsx
@@ -0,0 +1,85 @@
+import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { HeadingNode } from "@lexical/rich-text";
+import { Provider } from "@lexical/yjs";
+import { useSearchParams } from "next/navigation";
+import { WebsocketProvider } from "y-websocket";
+import { Doc } from "yjs";
+
+const theme = {
+  heading: {
+    h1: "mb-2 font-heading text-2xl font-bold",
+  },
+  layoutContainer: "PlaygroundEditorTheme__layoutContainer",
+  layoutItem: "PlaygroundEditorTheme__layoutItem",
+};
+
+function onError(error: Error): void {
+  console.error(error);
+}
+
+const WEBSOCKET_ENDPOINT = "ws://localhost:2345";
+const WEBSOCKET_SLUG = "playground";
+
+export default function Editor() {
+  const initialConfig = {
+    editorState: null,
+    namespace: "MyEditor",
+    theme,
+    onError,
+    nodes: [HeadingNode],
+  };
+
+  const queryParams = useSearchParams();
+  const WEBSOCKET_ID = queryParams.get("collabId") || "0";
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="relative border-2 rounded p-2 border-slate-500">
+        <CollaborationPlugin
+          id="main"
+          providerFactory={(id, yjsDocMap) => {
+            let doc = yjsDocMap.get(id);
+
+            if (doc === undefined) {
+              doc = new Doc();
+              yjsDocMap.set(id, doc);
+            } else {
+              doc.load();
+            }
+
+            console.log(
+              "Connecting to",
+              WEBSOCKET_ENDPOINT +
+                "/" +
+                WEBSOCKET_SLUG +
+                "/" +
+                WEBSOCKET_ID +
+                "/" +
+                id
+            );
+
+            // @ts-expect-error
+            return new WebsocketProvider(
+              WEBSOCKET_ENDPOINT,
+              WEBSOCKET_SLUG + "/" + WEBSOCKET_ID + "/" + id,
+              doc,
+              {
+                connect: false,
+              }
+            ) as Provider;
+          }}
+          shouldBootstrap={true}
+        />
+        <RichTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<></>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
+    </LexicalComposer>
+  );
+}
